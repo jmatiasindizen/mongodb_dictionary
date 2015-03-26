72f388617d9cdefa097d6a759311fe0babae4523
user <user@debian>
2015-03-26 09:23:25 -0400

    Adding changes for use collections as context manager and filter contents

--- collection.py
+++ collection.py
@@ -1,15 +1,31 @@
 #collection.py
 import pymongo
 
+	
 class Collection(object):
 	
 	def __init__(self, connection_string, db_name, collection_name):
 		self.connection = pymongo.MongoClient(connection_string)
 		self.db = eval('self.connection.' + db_name + '.' + collection_name)
-		self.cursor = self.db.find()
-		self.generator = self.generator_function()
-	
+		self.find_expression = None
+
+	def __enter__(self):
+		pass
+		
+	def __exit__(self, exc_type, exc_val, exc_tb):
+		pass
+		
+	def __call__(self, search_expression):
+		self.find_expression = search_expression
+		return self
+		
 	def __iter__(self):
+		if self.find_expression:
+			self.cursor = self.db.find(self.find_expression)
+		else:
+			self.cursor = self.db.find()
+		
+		self.generator = self.generator_function()
 		return self
 	
 	def __next__(self):
@@ -26,7 +42,7 @@ class Collection(object):
 			raise StopIteration()
 		
 	def __len__(self):
-		return self.cursor.count()
+		return self.db.count()
 		
 	def __getitem__(self, key):
 		return self.db.find_one({'_id': key})
@@ -36,11 +52,11 @@ class Collection(object):
 			value.update({'_id': key})
 		else:
 			value = {'_id': key, 'value': value}
-		
 		try:
 			self.db.insert(value)
 		except pymongo.errors.DuplicateKeyError:
 			print 'Error: Clave duplicada'
 		
 	def __delitem__(self, key):
+		print '__delitem__'
 		self.db.remove({'_id': key})
--- test_collection.py
+++ test_collection.py
@@ -13,3 +13,15 @@ class StoringTestCase(unittest.TestCase):
 	
 	def test_should_length_collection(self):
 		self.assertEqual( 10, len(self.collection))
+
+if __name__ == '__main__':
+	collection = Collection('mongodb://localhost', 'test_database', 'test_collection')
+	
+	#find all documents
+	for x in collection:
+		print str(x) + str(type(x))
+		
+	#filter documents
+	with collection({'_id':1}):
+		for x in collection:
+			print str(x) + str(type(x))
